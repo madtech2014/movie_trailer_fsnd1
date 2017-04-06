9a6c10186d521c2f8b17e794bee6caed68c13c0a
madtech2014 <masterteks@yahoo.com>
2017-04-06 11:55:28 -0400

    Code Cleanup
    Cleaning the code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,9 @@
 import media
 import fresh_tomatoes
-#toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life","http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=vwyZH85NQC4")
-#print(toy_story.storyline)
 
-#avatar = media.Movie("Avatar", "A marine on an alien planet", "Avatar Poster Link","https://www.youtube.com/watch?v=d1_JBMrrYw8")
-#print (avatar.storyline)
-#avatar.show_trailer()
+# create Movie class instances by compiling a list of favorite movies(title, storyline, link to poster art & link to trailer)
 
 shogun_assassin = media.Movie("Shogun Assassin", "A Samurai Warrior and his son flee the Emperor's assassination attempts", "https://img.discogs.com/itzaax05G4EOmQQ3vAlM4b0-Zlw=/fit-in/590x587/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2226989-1270991277.jpeg.jpg", "https://www.youtube.com/watch?v=bFOjbAQmIjY") 
-#shogun_assassin.show_trailer()
-#print (shogun_assassin.storyline)
 
 tombstone = media.Movie("Tombstone", "A sheriff and his brothers try to clean up a town", "http://img.moviepostershop.com/tombstone-movie-poster-1993-1010259614.jpg", "https://www.youtube.com/watch?v=IPS7xnVh4qk")
 
@@ -21,7 +15,8 @@ a_knights_tale = media.Movie("A Knight's Tale", "Story of a commoner who pretend
 
 bladerunner = media.Movie("Bladerunner", "A futeristic story of androids who go renegade when they have reached the end of their existence", "http://www.impawards.com/1982/posters/blade_runner_xlg.jpg", "https://www.youtube.com/watch?v=hiH4hxhKBmY")
 
+#call Movie __init__  & open_movie functions
+
 movies = [shogun_assassin, tombstone, blade, the_long_kiss_goodnight, a_knights_tale, bladerunner]
 fresh_tomatoes.open_movies_page(movies)
-#print (media.Movie.__name__)
-#print (media.Movie.__module__)
+
